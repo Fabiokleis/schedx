16296d3593bbebc7f1c45da342eddab6eefb2fb8
fabiokleis <fabiohkrc@gmail.com>
2024-08-03 02:37:33 -0300

    some tests

--- test/schedx_test.exs
+++ test/schedx_test.exs
@@ -3,14 +3,58 @@ defmodule SchedxTest do
   doctest Schedx
 
   test "test cron parser" do
-    assert Cronparser.parse("* * * * 1") != :invalid
-    assert Cronparser.parse("30 23 * * 6") != :invalid
-    assert Cronparser.parse("60 23 * * 6") == :invalid
-    assert Cronparser.parse("59 24 * * 6") == :invalid
-    assert Cronparser.parse("a 23 * * 6") == :invalid
-    assert Cronparser.parse("0 23 * * 7") == :invalid
-    assert Cronparser.parse("0 23 * 13 2") == :invalid
-    assert Cronparser.parse("0 23 0 * 6") == :invalid
-    assert Cronparser.parse("0 -1 1 * 4") == :invalid
+    assert Cronparser.parse("* * * * 1") == [
+             minute: :*,
+             hour: :*,
+             day: :*,
+             month: :*,
+             weekday: 1
+           ]
+
+    assert Cronparser.parse("30 23 * * 6") == [
+             minute: 30,
+             hour: 23,
+             day: :*,
+             month: :*,
+             weekday: 6
+           ]
+
+    assert Cronparser.parse("20 12") == [minute: 20, hour: 12]
+    assert Cronparser.parse("30 6,12,18") == [minute: 30, hour: [6, 12, 18]]
+    assert Cronparser.parse("30 6 1,15") == [minute: 30, hour: 6, day: [1, 15]]
+
+    assert Cronparser.parse("45 18 * * 0,2,4") == [
+             minute: 45,
+             hour: 18,
+             day: :*,
+             month: :*,
+             weekday: [0, 2, 4]
+           ]
+
+    assert Cronparser.parse("2 9-12 * * 1-5") == [
+             minute: 2,
+             hour: [9, 12],
+             day: :*,
+             month: :*,
+             weekday: [1, 5]
+           ]
+
+    # invalid periodic range
+    assert Cronparser.parse("2 9 * * 5-0") == {:weekday, :badcronrange}
+    assert Cronparser.parse("2 10-0 * * *") == {:hour, :badcronrange}
+    assert Cronparser.parse("2 1,30 * * *") == {:hour, :badcronrange}
+    assert Cronparser.parse("2 * * * 1,7") == {:weekday, :badcronrange}
+
+    # invalid literal range
+    assert Cronparser.parse("60 23 * * 6") == {:minute, :badcronrange}
+    assert Cronparser.parse("59 24 * * 6") == {:hour, :badcronrange}
+    assert Cronparser.parse("0 23 * * 7") == {:weekday, :badcronrange}
+    assert Cronparser.parse("0 23 * 13 2") == {:month, :badcronrange}
+    assert Cronparser.parse("0 23 0 * 6") == {:day, :badcronrange}
+
+    # unsupported syntax
+    assert Cronparser.parse("0 -1 1 * 4") == {:hour, :invalid}
+    assert Cronparser.parse("1-20 * * * 1,7") == {:minute, :invalid}
+    assert Cronparser.parse("a 23 * * 6") == {:minute, :invalid}
   end
 end
